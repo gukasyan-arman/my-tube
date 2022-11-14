c54f4b44c4d6f9a0eab49138416285402d961195
gukasyan-arman <gukasian.a.a@gmail.com>
2022-11-14 15:55:19 +0400

    SideBar styles & select category

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,37 +1,29 @@
 import React, { Component } from 'react';
 import style from './styles/SideBar.module.css';
 import { categories } from '../utils/constants'
+import classNames from 'classnames/bind'
 
 export function SideBar() {
+	const cx = classNames.bind(style);
+	let selectedCategory = ''
+
 	return (
 		<div className={style.container}>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
-			<div className={style.item}>Item</div>
+			{categories.map((category) => (
+				<button 
+					className={cx('item', {'itemSelected' : (selectedCategory === category.name)})}
+					key={category.name}
+					onClick={() => {
+						selectedCategory = category.name
+						console.log(selectedCategory)
+					}}
+
+				>
+					<span className={style.iconMargin}>{category.icon}</span>
+					<span>{category.name}</span>
+				</button>
+			))}
+
 			<div className={style.sideBarCopyright}>© 2022 Gukasyan-Arman</div>
 		</div>
 	);
